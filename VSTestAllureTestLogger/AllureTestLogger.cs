06ae737950618be4a7d7f34f5fb047fc407eb5f4
Daniel Kugel <mail4dk@gmail.com>
2014-10-12 19:57:12 +0300

    Added ReflectionOnlyContext attribute reading.

---
+++ VSTestAllureTestLogger/AllureTestLogger.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VSTestAllureTestLogger
+{
+    [ExtensionUri("logger://AllureTestLogger/v1")]
+    [FriendlyName("Allure")]
+    public class AllureTestLogger : ITestLogger
+    {
+        IDictionary<string, Assembly> mAssemblyMap = new Dictionary<string, Assembly>();
+
+        public void Initialize(TestLoggerEvents events, string testRunDirectory)
+        {
+            events.TestRunComplete += TestRunComplete;
+            events.TestResult += TestResult;
+
+            AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += ReflectionOnlyAssemblyResolve;
+        }
+
+        Assembly ReflectionOnlyAssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            return System.Reflection.Assembly.ReflectionOnlyLoad(args.Name);
+        }
+
+        void TestResult(object sender, TestResultEventArgs e)
+        {
+            string currentAssembly = Path.GetFileName(e.Result.TestCase.Source);
+            string fullyQualifiedName = e.Result.TestCase.FullyQualifiedName;
+            //Console.WriteLine("currentAssemblyPath: " + currentAssembly);
+            //Console.WriteLine("fullyQualifiedName: " + fullyQualifiedName);
+
+            Assembly assembly = LoadAssembly(currentAssembly);
+
+            IEnumerable<Type> testClasses = assembly.GetTypes().Where(type => type.GetCustomAttributesData().Where(cad => cad.AttributeType.FullName == typeof(TestClassAttribute).FullName).Any());
+
+            foreach (Type testClass in testClasses)
+            {
+                // no two methods can have the same fullyQualifiedName in a given assembly.
+                MethodInfo methodInfo = testClass.GetMethods().First(mi => (mi.DeclaringType.FullName + "." + mi.Name) == fullyQualifiedName);
+                
+                foreach (string category in GetCategories(methodInfo))
+                {
+                    Console.WriteLine("Category: " + category);
+                }
+
+                string description = GetDescription(methodInfo);
+                if (description != null)
+                    Console.WriteLine("Description: " + description);
+            }
+
+            Console.WriteLine();
+        }
+
+        void TestRunComplete(object sender, TestRunCompleteEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string GetDescription(MethodInfo methodInfo)
+        {
+            return ReflectionOnlyContextAttributeValueReader.TryGetAttributeConstructorArgument<DescriptionAttribute, string>(methodInfo).FirstOrDefault<string>();
+        }
+
+        private string GetOwner(MethodInfo methodInfo)
+        {
+            return ReflectionOnlyContextAttributeValueReader.TryGetAttributeConstructorArgument<OwnerAttribute, string>(methodInfo).FirstOrDefault<string>();
+        }
+
+        private IEnumerable<string> GetCategories(MethodInfo methodInfo)
+        {
+            return ReflectionOnlyContextAttributeValueReader.TryGetAttributeConstructorArgument<TestCategoryAttribute, string>(methodInfo);
+        }
+
+        private Assembly LoadAssembly(string assemblyName)
+        {
+            Assembly assembly;
+            if (!mAssemblyMap.TryGetValue(assemblyName, out assembly))
+            {
+                assembly = Assembly.ReflectionOnlyLoadFrom(assemblyName);
+                mAssemblyMap.Add(assemblyName, assembly);
+            }
+            return assembly;
+        }
+    }
+}
---
+++ VSTestAllureTestLogger/ReflectionOnlyContextAttributeValueReader.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSTestAllureTestLogger
+{
+    static class ReflectionOnlyContextAttributeValueReader
+    {
+        public static IEnumerable<TResult> TryGetAttributeProperty<TAttribute, TResult>(MethodInfo methodInfo, string propertyName)
+            where TAttribute : Attribute
+        {
+            Func<CustomAttributeData, AttributeValue<TResult>> extractor = (customAttributeData) =>
+            {
+                IList<CustomAttributeNamedArgument> properties = customAttributeData.NamedArguments;
+
+                foreach (CustomAttributeNamedArgument customAttributeNamedArgument in properties)
+                {
+                    if (customAttributeNamedArgument.MemberName == propertyName)
+                    {
+                        if (customAttributeNamedArgument.TypedValue.ArgumentType != typeof(TResult)) return AttributeValue<TResult>.NotFound;
+
+                        return new AttributeValue<TResult>((TResult)customAttributeNamedArgument.TypedValue.Value);
+                    }
+                }
+
+                return AttributeValue<TResult>.NotFound;
+            };
+            return GetAttributeValueInternal<TAttribute, TResult>(methodInfo, extractor);
+        }
+
+        public static IEnumerable<TResult> TryGetAttributeConstructorArgument<TAttribute, TResult>(MethodInfo methodInfo)
+            where TAttribute : Attribute
+        {
+            return TryGetAttributeConstructorArgument<TAttribute, TResult>(methodInfo, 0);
+        }
+
+        public static IEnumerable<TResult> TryGetAttributeConstructorArgument<TAttribute, TResult>(MethodInfo methodInfo, int constructorArgumentPosition)
+             where TAttribute : Attribute
+        {
+            Func<CustomAttributeData, AttributeValue<TResult>> extractor = (customAttributeData) =>
+            {
+                IList<CustomAttributeTypedArgument> constructorArguments = customAttributeData.ConstructorArguments;
+
+                if (constructorArgumentPosition >= constructorArguments.Count) return AttributeValue<TResult>.NotFound;
+
+                CustomAttributeTypedArgument customAttributeTypedArgument = constructorArguments[constructorArgumentPosition];
+
+                if (customAttributeTypedArgument.ArgumentType != typeof(TResult)) return AttributeValue<TResult>.NotFound;
+
+                return new AttributeValue<TResult>((TResult)customAttributeTypedArgument.Value);
+            };
+            return GetAttributeValueInternal<TAttribute, TResult>(methodInfo, extractor);
+        }
+
+        private static IEnumerable<TResult> GetAttributeValueInternal<TAttribute, TResult>(MethodInfo methodInfo, Func<CustomAttributeData, AttributeValue<TResult>> extractor)
+            where TAttribute : Attribute
+        {
+            IEnumerable<CustomAttributeData> customAttributeDataList = GetCustomAttributeData<TAttribute>(methodInfo);
+
+            if (!customAttributeDataList.Any())
+            {
+                return Enumerable.Empty<TResult>();
+            }
+
+            ICollection<TResult> result = new List<TResult>();
+            
+            foreach (CustomAttributeData customAttributeData in customAttributeDataList)
+            {
+                AttributeValue<TResult> value = extractor(customAttributeData);
+                
+                if (value == AttributeValue<TResult>.NotFound) continue;
+
+                result.Add(value.Value);
+            }
+
+            return result;
+        }
+
+        // a home cooked nullable style wrapper.
+        private class AttributeValue<T>
+        {
+            // marks an attribute value that wasn't found.
+            // could have used null but this is more readable.
+            public static readonly AttributeValue<T> NotFound = new AttributeValue<T>();
+
+            private AttributeValue()
+            {
+                Value = default(T);
+            }
+
+            public AttributeValue(T value)
+            {
+                Value = value;
+            }
+
+            public T Value { get; private set; }
+        }
+
+        private static IEnumerable<CustomAttributeData> GetCustomAttributeData<TAttribute>(MethodInfo methodInfo)
+        {
+            IList<CustomAttributeData> customAttributeDataList = CustomAttributeData.GetCustomAttributes(methodInfo);
+
+            return customAttributeDataList.Where<CustomAttributeData>(cad => cad.AttributeType.FullName == typeof(TAttribute).FullName);
+        }
+    }
+}
